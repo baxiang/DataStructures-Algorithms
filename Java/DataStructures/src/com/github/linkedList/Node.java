021060fa31480d3a95cc7cb05ec77017fb685041
baxiang <baxiang@roobo.com>
2020-03-22 19:51:10 +0800

    update skiplist

---
+++ Java/DataStructures/src/com/github/linkedList/Node.java
@@ -0,0 +1,11 @@
+package com.github.linkedList;
+
+public class Node {
+    public int data;
+    public Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
