b473968f110d08da31ee5ad044d7774e966f63a1
baxiang <baxiang@roobo.com>
2020-04-08 23:15:07 +0800

    面试题54. 二叉搜索树的第k大节点

--- Go/leetcode/300longest-increasing-subsequence/main.go
+++ Go/leetcode/300longest-increasing-subsequence/main.go
@@ -2,29 +2,29 @@ package main
 
 import "fmt"
 
-func max(a,b int)int{
-	if a>b {
+func max(a, b int) int {
+	if a > b {
 		return a
 	}
 	return b
 }
 
 func lengthOfLIS(nums []int) int {
-  dp :=make([]int,len(nums))
-  maxLen :=1
-  for i:=0;i<len(nums);i++{
-  	dp[i]=1
-  	for j:=0;j<i;j++{
-		if nums[j]<nums[i] {
-			dp[i] = max(dp[j]+1,dp[i])
+	dp := make([]int, len(nums))
+	maxLen := 0 // 处理数组为空情况
+	for i := 0; i < len(nums); i++ {
+		dp[i] = 1
+		for j := 0; j < i; j++ {
+			if nums[j] < nums[i] {
+				dp[i] = max(dp[j]+1, dp[i])
+			}
 		}
+		maxLen = max(maxLen, dp[i])
 	}
-	  maxLen =max(maxLen,dp[i])
-  }
-  return maxLen
+	return maxLen
 }
 
 //300. 最长上升子序列 https://leetcode-cn.com/problems/longest-increasing-subsequence/
 func main() {
-	fmt.Println(lengthOfLIS([]int{10,9,2,5,3,7,101,18}))
+	fmt.Println(lengthOfLIS([]int{10, 9, 2, 5, 3, 7, 101, 18}))
 }
