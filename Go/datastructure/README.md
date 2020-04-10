1aa92bb3794596e457e58202ac2681bd38fc192e
baxiang <baxiang@roobo.com>
2020-04-10 23:13:03 +0800

    queue

---
+++ Go/datastructure/README.md
@@ -0,0 +1,2 @@
+go test -v -run 测试函数名
+go test -v 
--- Go/leetcode/111minimum-depth-of-binary-tree/main.go
+++ Go/leetcode/111minimum-depth-of-binary-tree/main.go
@@ -1,5 +1,23 @@
 package main
+ type TreeNode struct {
+	     Val int
+	     Left *TreeNode
+	    Right *TreeNode
+}
+
+func min(a,b int)int{
+	if a>b {
+		return b
+	}
+	return a
+}
+func minDepth(root *TreeNode) int {
+     if root==nil{
+     	return 0
+	 }
+	return min(minDepth(root.Right),minDepth(root.Left))
+}
 
 func main() {
-	$END$
+	
 }
