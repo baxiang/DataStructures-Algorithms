5748956130cb76ffa1d1dbc1826be6531aae55b4
baxiang <baxiang@roobo.com>
2020-04-19 12:02:13 +0800

    leet code

--- Go/leetcode/17letter-combinations-of-a-phone-number/main.go
+++ Go/leetcode/17letter-combinations-of-a-phone-number/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func letterCombinations(digits string) []string {
+func letterCombinations1(digits string) []string {
 	if digits == "" || len(digits) == 0 {
 		return nil
 	}
@@ -12,7 +12,7 @@ func letterCombinations(digits string) []string {
 	list :=[]string{""}// 默认为空字符
 	for i:=0;i<len(digits);i++{
 		s :=nums[digits[i]-'0']
-		size :=len(list)
+		size :=len(list)// 原先的数组长度记录 涉及到底下的入队和出队
 		for j:=0;j<size;j++{
 			first := list[0]
 			list = list[1:]
@@ -23,6 +23,28 @@ func letterCombinations(digits string) []string {
 	}
 	return list
 }
+func letterCombinations(digits string) []string {
+	if digits == "" || len(digits) == 0 {
+		return nil
+	}
+	nums :=[]string{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"}
+	var res []string
+	res = append(res,"")
+	for i :=range digits{
+		 s := nums[digits[i]-'0']
+		 var temp []string
+		 for i:=0;i<len(s);i++{
+		 	for j:=0;j<len(res);j++{
+		 		temp = append(temp,res[j]+string(s[i]))
+
+			}
+		 }
+		 res = temp
+	}
+	return res
+}
+
+
 func main() {
-	fmt.Println(letterCombinations("23"))
+	fmt.Println(letterCombinations("234"))
 }
