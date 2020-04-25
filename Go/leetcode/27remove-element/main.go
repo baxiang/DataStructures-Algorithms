cf1a2af280af944e3658328c7de21e2a7271c2b8
baxiang <baxiang@roobo.com>
2020-04-25 22:39:06 +0800

    update

--- Go/leetcode/27remove-element/main.go
+++ Go/leetcode/27remove-element/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+
 //2n
 func removeElement(nums []int, val int) int {
 	var l []int
@@ -14,39 +15,36 @@ func removeElement(nums []int, val int) int {
 	}
 	return len(l)
 }
-//双指针写法
-func removeElement1(nums []int, val int) int {
-	slow := 0
-	quick :=0
-	for ;quick<len(nums);quick++{
-		if nums[quick]!=val {
-			if quick>slow {
-				nums[slow]=nums[quick]
-			}
-			slow++
-		}
-	}
-	return slow
-}
+
 func removeElement2(nums []int, val int) int {
 	var index = 0
 	for _,v :=range nums{
 		if val!=v{
-		   nums[index]= v
-		   index++
+			nums[index]= v
+			index++
 		}
 	}
 	return index
 }
-func main() {
-	l1 := []int{3,2,2,3}
-	r := removeElement2(l1,3)
-	fmt.Println(l1,r)
 
-	l2 :=[]int{0,1,2,2,3,0,4,2}
-	r = removeElement2(l2,2)
-	fmt.Println(l2,r)
-	////l3 :=[]int{0,1,2,2,3,0,4,2}
-	//r = removeElement2(l1,3)
-	//fmt.Println(l1,r)
+
+func removeElement3(nums []int, val int) int {
+	slow := 0
+	quick :=0
+	for quick<len(nums){
+		if nums[quick]!=val {
+			nums[slow]=nums[quick]
+			slow++
+		}
+		quick++
+	}
+	return slow
+}
+
+
+func main() {
+	fmt.Println(removeElement3([]int{3,2,2,3},3))//2
+	fmt.Println(removeElement3([]int{0,1,2,2,3,0,4,2},2))//5
+	fmt.Println(removeElement3([]int{2},3))//1
+	fmt.Println(removeElement3([]int{2},2))//0
 }
